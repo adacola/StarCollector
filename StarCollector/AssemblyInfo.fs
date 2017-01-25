10bb055df4400d4749663c7ac7d6d401a20afa8b
adacola <adacola255@gmail.com>
2017-01-25 10:45:27 +0900

    プロジェクト ファイルを追加します。

---
+++ StarCollector/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace StarCollector.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリに関する一般情報は、以下の属性セットによって
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[<assembly: AssemblyTitle("StarCollector")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("StarCollector")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// ComVisible を false に設定すると、COM コンポーネントがこのアセンブリ内のその型を認識
+// できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[<assembly: ComVisible(false)>]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります
+[<assembly: Guid("69f10b43-d075-4bb0-bebb-e302e1160b9b")>]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されます。:
+// 
+//       メジャー バージョン
+//       マイナー バージョン 
+//       ビルド番号
+//       リビジョン
+// 
+// すべての値を指定するか、下に示すように '*' を使用してビルドおよびリビジョン番号を
+// 既定値にすることができます。:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
